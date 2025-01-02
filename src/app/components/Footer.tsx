4bd4f95d20719ce5ab5b4e62616b88c41fa9aa77
Tanzeel Sarwar <tanzeelsarwar486@gmail.com>
2025-01-03 00:40:06 +0500

    Update Footer.tsx

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -15,7 +15,7 @@ export default function Footer() {
                 alt="RoyalsMC Logo"
                 width={40}
                 height={40}
-                className="h-10 w-auto"
+                className="h-14 w-auto"
               />
               <span className="text-2xl font-bold text-[#7DAC52]">RoyalsMC</span>
             </Link>
